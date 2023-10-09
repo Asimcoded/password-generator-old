bd33d8d7868a42d2bec0c1f7560c36a2a286b38b
Asim Das <asim71190@gmail.com>
2023-10-09 20:36:50 +0530

    Added javascript function

--- script.js
+++ script.js
@@ -10,8 +10,38 @@ const lowerCase = upperCase.toLowerCase();
 const number = "1234567890";
 const symbol = "~!@#$%^&*()_+=-[]{}\|/?><";
 
-function createPassword(){
-    let length = lengthBox.value
-    console.log(length);    
+function createPassword() {
+    // let length = lengthBox.value;
+    let password = ""
+    let allChar = "";
+    if (upperCaseBox.checked) {
+        allChar += upperCase;
+    }
+    if (lowerCaseBox.checked) {
+        allChar += lowerCase;
+    }
+    if (numberBox.checked) {
+        allChar += number;
+    }
+    if (symbolBox.checked) {
+        allChar += symbol;
+    }
+
+    if (upperCaseBox.checked == false && lowerCaseBox.checked == false && numberBox.checked == false && symbolBox.checked == false) {
+        console.log("Nothing is checked");
+    }
+    // else{
+    //     while(lengthBox.value > password.length){
+    //     }
+    //     // console.log(allChar);
+    //     console.log(password);
+    //     // console.log(length); 
+    // }
+    else {
+        for (let i = 0; i < lengthBox.value; i++) {
+            password += allChar[Math.floor(Math.random() * allChar.length)];
+        }
+        passwordBox.value = password
+    }
 }
-document.querySelector('#generate').addEventListener('click',createPassword)
+document.querySelector('#generate').addEventListener('click', createPassword)
